398a6cb2f6c9e05fe66572e716f0cc27b6408e46
Shreyas <shreyu86@gmail.com>
2012-06-19 17:39:39 -0500

    Refactored code to make PEP8 friendly :smile:

--- bin/run_pipeline.py
+++ bin/run_pipeline.py
@@ -3,17 +3,23 @@ from pipes.lib.pipeline import Pipeline
 import logging
 import argparse
 parser = argparse.ArgumentParser(description='Proccess String via a pipeline')
-parser.add_argument("-s", action = "store", dest = "inputstring", help = "String to Be Proccessed",default="Hello Python")
-parser.add_argument("-c", action = "store", dest = "configfile", help = "Configfile Containing Instructions",default="config.yaml")
+parser.add_argument("-s", action="store", dest="inputstring",
+                    help="String to Be Proccessed", default="Hello Python")
+parser.add_argument("-c", action="store", dest="configfile",
+                    help="Configfile Containing Instructions",
+                    default="config.yaml")
 args = parser.parse_args()
+
+
 def main():
-  input = {}
-  logging.basicConfig(level='INFO')
-  input['original'] = args.inputstring
-  pipeline = Pipeline(args.configfile)
-  logging.info('Input is = {0}'.format(input))
-  pipeline.run(input)
-  logging.info('Input is now = {0}'.format(input))
-if __name__=="__main__":
-  main()
-    
+    input = {}
+    logging.basicConfig(level='INFO')
+    input['original'] = args.inputstring
+    pipeline = Pipeline(args.configfile)
+    logging.info('Input is = {0}'.format(input))
+    pipeline.run(input)
+    logging.info('Input is now = {0}'.format(input))
+
+
+if __name__ == "__main__":
+    main()
