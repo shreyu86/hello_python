6ed9b23af6e7060c09d66c600987072bea00c5a4
Shreyas <shreyu86@gmail.com>
2012-06-19 19:47:28 -0500

    Pipeline is now PEP8 styled

--- pipes/lib/pipeline.py
+++ pipes/lib/pipeline.py
@@ -4,17 +4,17 @@ import logging
 import datetime
 import yaml
 import hashlib
-### Edited this step to include an config file in YAML 
+### Edited this step to include an config file in YAML
 ### which contains the string ID to be acted on and which pipelines to execute.
+
 
 class Pipeline(object):
 
     #STEPS = ['steps.uppercase','steps.duplicate','steps.adddate']
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.load_components(filename)
 
-   
     def create_object(self, module, object_name, **kwargs):
 
         mod = importlib.import_module(module)
@@ -23,47 +23,48 @@ class Pipeline(object):
             obj = getattr(mod, object_name)(**kwargs)
         else:
             obj = getattr(mod, object_name)()
-
         return obj
-        
-    def load_components(self,filename):
+
+    def load_components(self, filename):
         logging.info('Loading components')
         self.configfile = filename
         self.pipeline = []
-        self.STEPS    = []
+        self.STEPS = []
         self.configdict = {}
         self.procstring = None
         h = hashlib.new('ripemd160')
         try:
-	  filehandler = open (self.configfile,'r')
-	  yamlread = yaml.load(filehandler)
-	  self.STEPS = yamlread['pipelinesteps']
-	  self.procstring = yamlread['proc_string_id']
-	except:
-	  logging.exception("Cannot load the config file")
-        logging.info('Config file loaded')
+            filehandler = open(self.configfile, 'r')
+            yamlread = yaml.load(filehandler)
+            self.STEPS = yamlread['pipelinesteps']
+            self.procstring = yamlread['proc_string_id']
+        except:
+            logging.exception("Cannot load the config file")
+            logging.info('Config file loaded')
         for step in self.STEPS:
-	    splitstep_id = step.split(',')
-	    step_fullname = splitstep_id[0]
-	    step_displayname = splitstep_id[1]
+            splitstep_id = step.split(',')
+            step_fullname = splitstep_id[0]
+            step_displayname = splitstep_id[1]
             logging.info('Initializing {0}'.format(step_fullname))
             try:
                 step_object = self.create_object(step_fullname, 'Step')
-                self.pipeline.append(getattr(step_object,'run'))
-                h.update(str(getattr(step_object,'run')))
-                self.configdict[h.hexdigest()]=step_displayname
+                self.pipeline.append(getattr(step_object, 'run'))
+                h.update(str(getattr(step_object, 'run')))
+                self.configdict[h.hexdigest()] = step_displayname
             except:
-                logging.warning('Error occured loading {0}'.format(step_fullname))
+                logging.warning('Error occured loading {0}'
+                .format(step_fullname))
                 logging.exception('Error loading pipeline component')
+
     def run(self, input):
         h = hashlib.new('ripemd160')
         #print self.configdict
         for component in self.pipeline:
             try:
-	        #print "After "+str(component)
-	        h.update(str(component))
-	      ## Add the string on which the pipeline has to executed
+                #print "After "+str(component)
+                h.update(str(component))
+                ## Add the string on which the pipeline has to executed
                 currentstep = self.configdict[h.hexdigest()]
-                component(input,self.procstring,currentstep)
+                component(input, self.procstring, currentstep)
             except:
                 logging.exception('Unable to run component')
