99d17f2f0280c83a248d81770085822ebe4ea163
Shreyas <shreyu86@gmail.com>
2012-06-19 21:17:52 -0500

    Changed setup.py to reflect current version of application

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 
 from setuptools import setup
 setup(name='PipeProject',
-      version='0.1',
+      version='0.1.1',
       description='Pipes for plumbing at Shopply',
       author='Shreyas Karnik',
       author_email='shreyu86@gmail.com',
       url='https://github.com/shreyu86/hello_python/',
       packages=['pipes', 'pipes.lib', 'pipes.steps', 'pipes.test'],
       package_data={'': ['*.yaml']},
-      setup_requires=['nose>=1.0'],
       scripts=['./bin/run_pipeline.py'],
       install_requires=['PyYAML', 'datetime', 'hashlib'],
       )
